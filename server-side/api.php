c54c3ce7abc9b0fdfadf7d4c9da8cc60063d0b49
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-12 16:31:00 +0100

    server-side / connected backend and frontend

--- server-side/api.php
+++ server-side/api.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: http://localhost:80");
+    header("Access-Control-Allow-Origin: http://localhost:5173");
     header("Access-Control-Allow-Headers: X-Requested-With");
     
     header('Content-Type: application/json');
