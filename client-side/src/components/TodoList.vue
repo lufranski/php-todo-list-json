44688e2e53e592e220be522aa74ac21400f73c8e
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-12 17:36:46 +0100

    client-side / implemented some css

--- client-side/src/components/TodoList.vue
+++ client-side/src/components/TodoList.vue
@@ -27,9 +27,12 @@ export default{
                 'newTask' : this.newTask
             }};
 
-            axios.get(this.apiUrl + 'api-new-task.php', par).then(() => {
-                this.getTodoList();
-            });
+            if(this.newTask != ''){
+                
+                axios.get(this.apiUrl + 'api-new-task.php', par).then(() => {
+                    this.getTodoList();
+                });
+            }
         }
     },
     mounted() {
@@ -40,19 +43,73 @@ export default{
 
 <template>
 
-    <!-- TEST -->
-    <h1>TODOLIST</h1>
+    <div>
+        <h1>TODOLIST</h1>
+    </div>
 
     <ul>
         <li v-for="task in this.todoList">{{task.name}}</li>
     </ul>
 
     <form @submit="addTask">
-        <input type="text" name="newTask" v-model="newTask">
-        <input type="submit" value="ADD TASK">
+        <input type="text" name="newTask" v-model="newTask" id="new-task">
+        <input type="submit" value="ADD TASK" id="button">
     </form>
 </template>
 
 <style scoped>
+div {
+    width: 40%;
+    margin: 50px auto;
+    text-align: center;
+    color: #FB2576;
+}
+
+ul {
+    width: 20%;
+    height: 400px;
+    overflow-y: auto;
+    margin: 0 auto;
+
+}
+
+li {
+    background-color: #FB2576;
+    margin-bottom: 1rem;
+    padding: 5px;
+    border-radius: 16px 0 16px 0;
+    color: black;
+    font-weight: 900;
+}
+
+form {
+    width: 35%;
+    margin: 0 auto;
+}
+
+#new-task {
+    width: 80%;
+    margin-right: 20px;
+    padding: .5rem;
+    border-radius: 0 16px 0 16px;
+    transition: .4s;
+}
+
+#new-task:focus{
+    border-radius: 16px 0 16px 0;
+}
+
+#button {
+    padding: .5rem;
+    border-radius: 0 16px 0 16px;
+    transition: .4s;
+}
+
+#button:focus {
+    border-radius: 16px 0 16px 0;
+}
+
+
+
 </style>
 
