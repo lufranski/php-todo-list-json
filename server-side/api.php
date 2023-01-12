0d35f05e4223adbf248705a18cff8aa11bb19912
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-12 15:33:48 +0100

    server-side / created toDoList array and todo.json

---
+++ server-side/api.php
@@ -0,0 +1,28 @@
+<?php
+
+    $toDoList = [
+        [
+            'name' => 'HTML',
+            'completed' => true
+        ],
+        [
+            'name' => 'CSS',
+            'completed' => true
+        ],
+        [
+            'name' => 'Responsive Design',
+            'completed' => true
+        ],
+        [
+            'name' => 'Javascript',
+            'completed' => true
+        ],
+        [
+            'name' => 'PHP',
+            'completed' => false
+        ],
+        [
+            'name' => 'Laravel',
+            'completed' => false
+        ],
+    ];
