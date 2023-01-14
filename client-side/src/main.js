860c2e3305f1303ad4e8ec4bd837abb769215198
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-14 17:20:39 +0100

    Bonus 4 / client-side / installed fontawesome

--- client-side/src/main.js
+++ client-side/src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrashCan)
+
+createApp(App)
+.component('font-awesome-icon' , FontAwesomeIcon)
+.mount('#app')
